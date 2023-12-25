42e6646e7c8c8cdf14d16bb9d654f30f1a179f0b
Shskjssk10 <cadentohjunyi@gmail.com>
2023-12-25 17:39:34 +0800

    Updated data atributes in HTML and redid JS skeleton

--- Calculator-App/calculator-app.js
+++ Calculator-App/calculator-app.js
@@ -1,8 +1,70 @@
-const buttons = document.querySelectorAll(".row span");
-const display = document.querySelector(".display");
+class Calculator{
+    constructor(previousOperand, currentOperand){
+        this.previousOperand = previousOperand;
+        this.currentOperand = currentOperand;
+    }
+    clear(){
 
-const updateDisplay = () => {
-    
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(){
+        
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    getDisplayNumber(){
+
+    }
+
+    updateDisplay(){
+
+    }
+
 }
 
-document.addEventListener("keydown");
+const numberButtons = document.querySelectorAll('[data-numbers]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const backSpaceButton = document.querySelector('[data-backspace]');
+const operandButtons = document.querySelectorAll('[data-operands]');
+const equalSignButton = document.querySelector('[data-equal-sign]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+console.log(currentOperandTextElement);
+let previousOperand;
+
+const cadenCalculator = new Calculator(currentOperandTextElement, previousOperand);
+
+numberButtons.forEach(button => {
+    document.addEventListener('click', () => {
+        cadenCalculator.appendNumber(button.innerHTML);
+        cadenCalculator.updateDisplay();
+    })
+})
+equalSignButton.addEventListener('click', button => {
+    cadenCalculator.compute();
+    cadenCalculator.updateDisplay();
+})
+backSpaceButton.addEventListener('click', button => {
+    cadenCalculator.delete();
+    cadenCalculator.updateDisplay();
+})
+operandButtons.forEach(button => {
+    document.addEventListener('click', () => {
+        cadenCalculator.chooseOperation(button.innerHTML);
+        cadenCalculator.updateDisplay();
+    })
+})
+allClearButton.addEventListener('click', button => {
+    cadenCalculator.clear();
+    cadenCalculator.updateDisplay();
+})
